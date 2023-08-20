1ba436b44e568d1477891d29d11ab3397169b213
qianwen@xsky.com <qianwen@xsky.com>
2023-08-20 19:49:26 +0800

    feat: 初始化二维地图
    Signed-off-by: qianwen@xsky.com <qianwen@xsky.com>

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { onMounted } from "vue"
+import Map from "@arcgis/core/Map.js"
+import MapView from "@arcgis/core/views/MapView.js"
+
+onMounted(() => {
+  const map = new Map({
+    basemap: "topo-vector",
+  })
+  const view = new MapView({
+    container: "map",
+    map: map,
+    zoom: 4,
+    center: [15, 65],
+  })
+})
+</script>
+
+<template>
+  <div id="map"></div>
+</template>
+
+<style scoped>
+#map {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+}
+</style>
