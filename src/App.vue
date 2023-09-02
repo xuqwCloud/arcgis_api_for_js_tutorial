06311ce87a18246322c3d127f1da10196c7fe864
qianwen@xsky.com <qianwen@xsky.com>
2023-09-03 07:13:14 +0800

    feat: add feat/module_2_operate_map branch
    Signed-off-by: qianwen@xsky.com <qianwen@xsky.com>

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,9 @@
 import { onMounted } from "vue"
 import Map from "@arcgis/core/Map.js"
 import MapView from "@arcgis/core/views/MapView.js"
+import AreaMeasurement2D from "@arcgis/core/widgets/AreaMeasurement2D.js"
+import BasemapToggle from "@arcgis/core/widgets/BasemapToggle.js"
+import Home from "@arcgis/core/widgets/Home.js"
 
 onMounted(() => {
   const map = new Map({
@@ -10,9 +13,49 @@ onMounted(() => {
   const view = new MapView({
     container: "map",
     map: map,
-    zoom: 4,
-    center: [15, 65],
+    // zoom: 12, // 缩放级别
+    // center: [104.07, 30.66], // 中心点坐标
+    rotation: 0, // 倾斜角
   })
+
+  view.zoom = 8
+  view.center = [104.07, 30.66]
+
+  // 5秒后跳转到北京
+  // setTimeout(() => {
+  //   view.goTo({
+  //     center: [116.38, 39.87],
+  //   })
+  // }, 5000)
+
+  // 地图绑定点击事件
+  view.on("click", function (event) {
+    console.log("click", event.mapPoint)
+  })
+
+  // 添加面积测量工具 2D
+  const measurementWidget = new AreaMeasurement2D({
+    view: view,
+  })
+  view.ui.add(measurementWidget, "top-right")
+
+  // 添加底图切换
+  const basemapToggle = new BasemapToggle({
+    view: view,
+    nextBasemap: "osm",
+  })
+  view.ui.add(basemapToggle, "bottom-right")
+
+  // 添加 Home 按钮
+  const homeWidget = new Home({
+    view: view,
+  })
+
+  view.ui.add(homeWidget, "top-left")
+  // 5秒后销毁 Home 按钮
+  setTimeout(() => {
+    homeWidget.destroy()
+  }, 5000)
 })
 </script>
 
