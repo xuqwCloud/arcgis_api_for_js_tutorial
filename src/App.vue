d8dbd6827f236a52f9dc5ec7aab24984f9f47d49
qianwen@xsky.com <qianwen@xsky.com>
2023-10-02 08:18:36 +0800

    feat: add feat/module_5_map_event
    Signed-off-by: qianwen@xsky.com <qianwen@xsky.com>

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,23 @@ onMounted(() => {
     zoom: 4,
     center: [15, 65],
   })
+
+  // 地图点击事件
+  const clickObj = view.on("click", function (event) {
+    // 在此处处理单击事件
+    console.log("点击坐标：", event.mapPoint)
+  })
+
+  // 地图双击事件
+  view.on("double-click", function (event) {
+    // 在此处处理双击事件
+    console.log("双击坐标：", event.mapPoint)
+  })
+
+  // 10s 之后卸载地图点击事件
+  setTimeout(() => {
+    clickObj.remove()
+  }, 10000)
 })
 </script>
 
